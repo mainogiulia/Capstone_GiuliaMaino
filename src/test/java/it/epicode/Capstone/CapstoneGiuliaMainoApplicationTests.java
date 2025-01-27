d14a3e8a87c1e631967c9fc16364d7af2eb2a7a8
mainogiulia <giuliamaino97@gmail.com>
2025-01-27 10:54:34 +0100

    Initial commit

---
+++ src/test/java/it/epicode/Capstone/CapstoneGiuliaMainoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneGiuliaMainoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
